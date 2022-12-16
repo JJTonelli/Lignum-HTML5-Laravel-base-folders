ba1303f10af60f66da853bf31e1b820f1154bca1
JJTonelli <juanjo_tonelli@hotmail.com>
2022-12-16 11:01:31 -0300

    migrations and model relationships

---
+++ 03-MVC-Laravel/laravel-bootcamp/database/migrations/2022_12_16_131054_create_peliculas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('año');
+            $table->string('titulo');
+            $table->integer('duracion');
+            $table->string('sinopsis');
+            $table->string('imagen');
+            $table->unsignedBigInteger('actorPrincipalId');
+            $table->foreign('actorPrincipalId')->references('id')->on('actors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peliculas');
+    }
+};
