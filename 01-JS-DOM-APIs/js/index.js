e08ee4e820fcfe1e545a252d4dcd15d7501918a0
JJTonelli <juanjo_tonelli@hotmail.com>
2022-12-16 05:46:08 -0300

    exercise 1.2

--- 01-JS-DOM-APIs/js/index.js
+++ 01-JS-DOM-APIs/js/index.js
@@ -1,18 +1,21 @@
 window.onload = () => {
   let opacity = 0;
-  let intervalID = 0;
-  setInterval(show, 200);
+  let intervalID = setInterval(show, 200);
+
+  function show() {
+    let section = document.getElementById('fade-in');
+    opacity = Number(
+      window.getComputedStyle(section).getPropertyValue('opacity')
+    );
+    if (opacity < 1) {
+      opacity = opacity + 0.1;
+      section.style.opacity = opacity;
+    } else {
+      clearInterval(intervalID);
+    }
+  }
 };
 
-function show() {
-  let section = document.getElementById('fade-in');
-  opacity = Number(
-    window.getComputedStyle(section).getPropertyValue('opacity')
-  );
-  if (opacity < 1) {
-    opacity = opacity + 0.1;
-    section.style.opacity = opacity;
-  } else {
-    clearInterval(intervalID);
-  }
+function buttonAlert() {
+  alert('Presionaste el boton!');
 }
